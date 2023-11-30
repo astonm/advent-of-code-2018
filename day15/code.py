b288f648e42b0f4cfce03012b0c2e6f1a8bb41f9
Aston Motes <astonm@gmail.com>
2023-11-29 23:05:30 -0800

    add day15 part2

--- day15/code.py
+++ day15/code.py
@@ -16,13 +16,18 @@ def cli():
 def part1(input):
     g = Grid.from_string(input.read())
     units = get_units(g)
+    last_round = run_game(g, units)
+    return submit.part(1, last_round * sum(u.hp for u in units if u.alive))
+
+
+def run_game(g, units):
     rounds = count(1)
 
     for round in rounds:
         for u in units:
             live_units = [u for u in units if u.alive]
             if game_over(live_units):
-                return submit.part(1, (round - 1) * sum(u.hp for u in units if u.alive))
+                return round - 1
 
             if u.alive:
                 take_turn(u, g, live_units)
@@ -34,9 +39,23 @@ def part1(input):
 @cli.command()
 @click.argument("input", type=click.File())
 def part2(input):
-    data = [process_line(l) for l in read_file(input)]
-    pprint(locals())
-    return submit.part(2, None)
+    board = input.read()
+
+    prev_good = False
+    for elf_ap in tqdm(range(3, 200)):
+        print(elf_ap)
+        g = Grid.from_string(board)
+        units = get_units(g, elf_ap=elf_ap)
+        n_elves_start = sum(1 for u in units if u.v == "E")
+
+        last_round = run_game(g, units)
+        n_elves_end = sum(1 for u in units if u.alive and u.v == "E")
+        good = n_elves_end == n_elves_start
+
+        if good and not prev_good:
+            score = last_round * sum(u.hp for u in units if u.alive)
+            return submit.part(2, last_round * sum(u.hp for u in units if u.alive))
+        prev_good = good
 
 
 @dataclass
@@ -55,11 +74,12 @@ def point_order(x):
         return x[1], x[0]
 
 
-def get_units(g):
+def get_units(g, elf_ap=3):
     out = []
     for p, v in zip(g.walk_coords(), g.walk()):
         if v in "EG":
-            out.append(Unit(p, v, ap=3, hp=200, alive=True))
+            ap = 3 if v == "G" else elf_ap
+            out.append(Unit(p, v, ap=ap, hp=200, alive=True))
     return out
 
 
@@ -96,19 +116,17 @@ def move(u, open_in_range, g):
 
 def shortest_path_to_any(start, goals, g):
     q = [start]
-    seen = set()
     backlinks = {}
     while q:
         p = q.pop(0)
 
-        seen.add(p)
         if p in goals:
             curr_path = get_path(p, backlinks)
             return curr_path[1:]
 
         for dx, dy in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
             np = p[0] + dx, p[1] + dy
-            if g.get(*np) == "." and np not in seen:
+            if g.get(*np) == "." and np not in backlinks:
                 backlinks[np] = p
                 q.append(np)
 
